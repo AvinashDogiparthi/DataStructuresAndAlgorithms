6eebe349b6a28c7c215517f93a80f1c59d5a758f
Avinash-Dev-CFG <dogiparthi.naga@amadeus.com>
2025-05-20 00:38:28 +0530

    TwoSumII : using two pointers appraoch

--- ProblemSolvingUsingPatterns/src/Two_Pointers_Pattern/A_TwoSum_Easy/A_SolutionUsingTwoPointers.java
+++ ProblemSolvingUsingPatterns/src/Two_Pointers_Pattern/A_TwoSum_Easy/A_SolutionUsingTwoPointers.java
@@ -1,61 +1,59 @@
-package Two_Pointers_Pattern.A_TwoSum;
+package Two_Pointers_Pattern.A_TwoSum_Easy;
 
 import java.util.Arrays;
 
 public class A_SolutionUsingTwoPointers {
 
     public int[] twoSum(int[] nums, int target) {
-
         int[] copiedArray = Arrays.copyOf(nums,nums.length);
         Arrays.sort(nums);
+
         int pointerA = 0;
         int pointerB = nums.length-1;
-        int[] returnableArray = new int[2];
-        int iterableSum = 0;
+
+        int sum = 0;
         boolean found = false;
+        int[] returnableArray = new int[2];
 
         // Time Complexity : O(N)
         while(pointerA < pointerB){
 
-            //baseCondition
-            iterableSum = nums[pointerA] +nums[pointerB];
-            if(iterableSum==target){
+            sum = nums[pointerA]+nums[pointerB];
+
+            // base condition
+            if( sum == target){
 
                 int pointerAValueIndex = searchElement(nums[pointerA],copiedArray,-1);
-                int pointerBValueIndex = searchElement(nums[pointerB],copiedArray,pointerAValueIndex);
+                int pointerBValueIndex = searchElement(nums[pointerB], copiedArray,pointerAValueIndex);
 
-                if(pointerAValueIndex == pointerBValueIndex){
-                    pointerBValueIndex = pointerBValueIndex + 1;
-                }
                 returnableArray[0] = pointerAValueIndex;
                 returnableArray[1] = pointerBValueIndex;
                 found = true;
             }
 
-
-            if(iterableSum<target){
+            if(sum<target){
                 pointerA++;
             } else {
                 pointerB--;
             }
         }
 
-        return found ? returnableArray : new int[]{-1,-1};
+        return found?returnableArray:new int[]{-1,-1};
     }
 
-
     // Time Complexity : O(N)
-    int searchElement(int element, int[] array, int ignorableIndex){
+    /*
+      ignorableIndex to skip duplicate element index in second iteration
+     */
+    private int searchElement(int element, int[] array, int ignorableIndex){
+        int index = Integer.MIN_VALUE;
 
-        int elementIndex = Integer.MIN_VALUE;
-
-        for(int i = 0 ;i<array.length;i++){
+        for(int i = 0;i<array.length;i++){
             if(array[i] == element && i != ignorableIndex){
-                return i;
+                index = i;
             }
         }
 
-        return elementIndex;
-
+        return index;
     }
 }
---
+++ ProblemSolvingUsingPatterns/src/Two_Pointers_Pattern/A_TwoSum_Easy/C_OptimizedSolutionUsingHashMap.java
@@ -0,0 +1,29 @@
+package Two_Pointers_Pattern.A_TwoSum_Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C_OptimizedSolutionUsingHashMap {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> mapOfNumbers = new HashMap<Integer, Integer>();
+        int[] returnableArray = new int[2];
+        boolean found = false;
+
+        for(int i = 0 ;i<nums.length;i++){
+
+            int currentValue = nums[i];
+
+            // storing value as key and it's value as index
+            if(mapOfNumbers.containsKey(target-currentValue)){
+                returnableArray[0] = i;
+                returnableArray[1] = mapOfNumbers.get(target-currentValue);
+                found = true;
+            } else {
+                mapOfNumbers.put(currentValue,i);
+            }
+        }
+
+        return found?returnableArray:new int[]{-1,-1};
+    }
+}
