413adc8e28800ea029578bbc3a1644e1e8612574
AvinashDogiparthi <dvsubharao1963@gmail.com>
2025-05-30 22:19:27 +0530

    Slow pointers and Fast pointers : Problem solving

--- ProblemSolvingUsingPatterns/src/Two_Pointers_Pattern/D_Remove_Duplicate_LinkedList/ListNode.java
+++ ProblemSolvingUsingPatterns/src/Two_Pointers_Pattern/D_Remove_Duplicate_LinkedList/ListNode.java
@@ -3,10 +3,13 @@ package Two_Pointers_Pattern.D_Remove_Duplicate_LinkedList;
 public class ListNode {
     public int val;
     public ListNode next;
+<<<<<<< Updated upstream
 
     public ListNode(int val) {
         this.val = val;
     }
+=======
+>>>>>>> Stashed changes
 
     public ListNode(int val, ListNode next) {
         this.val = val;
