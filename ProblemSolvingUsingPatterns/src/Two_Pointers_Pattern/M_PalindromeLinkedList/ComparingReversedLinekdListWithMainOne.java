92734afa11eb6a21b3a25e37b228aa8e3c2e3ccd
Avinash-Dev-CFG <dogiparthi.naga@amadeus.com>
2025-06-09 21:37:13 +0530

    New problem solving using two pointers

---
+++ ProblemSolvingUsingPatterns/src/Two_Pointers_Pattern/M_PalindromeLinkedList/ComparingReversedLinekdListWithMainOne.java
@@ -0,0 +1,50 @@
+package Two_Pointers_Pattern.M_PalindromeLinkedList;
+
+import Two_Pointers_Pattern.D_Remove_Duplicate_LinkedList.ListNode;
+
+public class ComparingReversedLinekdListWithMainOne {
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode tempNode = head;
+        ListNode reverseNode = reverseLinkedList(copyList(tempNode));
+
+        while(head != null){
+            if(reverseNode.val != head.val){
+                return false;
+            }
+
+            reverseNode = reverseNode.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+    public ListNode reverseLinkedList(ListNode head){
+
+        ListNode previous = null;
+
+        while(head != null){
+            ListNode nextNode = head.next;
+            head.next = previous;
+
+            previous = head;
+            head = nextNode;
+        }
+
+        return previous;
+    }
+
+    private ListNode copyList(ListNode head) {
+        if (head == null) return null;
+        ListNode newHead = new ListNode(head.val);
+        ListNode current = newHead;
+        head = head.next;
+        while (head != null) {
+            current.next = new ListNode(head.val);
+            current = current.next;
+            head = head.next;
+        }
+        return newHead;
+    }
+}
