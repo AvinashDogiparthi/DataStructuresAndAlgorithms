b29ec6951dc50d3a05910c8a1d11f76d366bd739
Avinash-Dev-CFG <dogiparthi.naga@amadeus.com>
2025-06-16 16:43:49 +0530

    Problem solving using dynamic sized window

--- ProblemSolvingUsingPatterns/src/Sliding_Window_Pattern/FixedSizeWindow/A_MaximumAverageSubarrayI/SolutionUsingFixedWindow.java
+++ ProblemSolvingUsingPatterns/src/Sliding_Window_Pattern/FixedSizeWindow/A_MaximumAverageSubarrayI/SolutionUsingFixedWindow.java
@@ -3,21 +3,22 @@ package Sliding_Window_Pattern.FixedSizeWindow.A_MaximumAverageSubarrayI;
 public class SolutionUsingFixedWindow {
 
     public double findMaxAverage(int[] nums, int k) {
-        double maxAverage = 0;
-        double sum = 0;
+
+        int maximumSum = 0;
 
         for(int i = 0;i<k;i++){
-            sum = sum + nums[i];
+            maximumSum += nums[i];
         }
 
-        maxAverage = sum/k; //initial k indices sum
+        int currentSum = maximumSum;
 
         for(int i = k;i<nums.length;i++){
-            sum = sum + nums[i] - nums[i-k];
-            double currentSumAverage = sum/k;
-            maxAverage = Math.max(maxAverage,currentSumAverage);
+
+            currentSum = currentSum + nums[i] - nums[i-k];
+            maximumSum = Math.max(currentSum,maximumSum);
         }
 
-        return maxAverage;
+        return (double) maximumSum/k;
+
     }
 }
