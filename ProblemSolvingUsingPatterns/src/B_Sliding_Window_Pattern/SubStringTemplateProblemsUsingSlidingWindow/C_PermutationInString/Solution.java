b2eba5ecfa9b8a368c81c253e373268875125cbb
AvinashDogiparthi <dvsubharao1963@gmail.com>
2025-08-10 07:37:00 +0530

    Sliding window template solution for permutation in a string leet code question

---
+++ ProblemSolvingUsingPatterns/src/B_Sliding_Window_Pattern/SubStringTemplateProblemsUsingSlidingWindow/C_PermutationInString/Solution.java
@@ -0,0 +1,47 @@
+package B_Sliding_Window_Pattern.SubStringTemplateProblemsUsingSlidingWindow.C_PermutationInString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public boolean checkInclusion(String s1, String s2) {
+
+        if(s1.length() > s2.length()) return false;
+
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c : s1.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        int counter = map.size();
+        int begin = 0;
+        int end = 0;
+        int len = Integer.MAX_VALUE;
+
+        while(end < s2.length()){
+
+            char c = s2.charAt(end);
+
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)-1);
+                if(map.get(c) == 0) counter--;
+            }
+            end++;
+
+            while(counter == 0){
+                char beginChar = s2.charAt(begin);
+                if(map.containsKey(beginChar)){
+                    map.put(beginChar,map.get(beginChar)+1);
+                    if(map.get(beginChar) > 0) counter++;
+                }
+
+                // added this if condition to check the length
+                if(end-begin == s1.length()) return true;
+                begin++;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ ProblemSolvingUsingPatterns/src/B_Sliding_Window_Pattern/SubStringTemplateProblemsUsingSlidingWindow/D_LongestSubStringWithoutRepeatingCharacters/README.md
@@ -0,0 +1,17 @@
+# Longest Substring Without Repeating Characters
+
+## Problem Link
+[LeetCode - Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/)
+
+---
+
+## Problem Description
+
+**Difficulty:** Medium  
+**Topics:** String, Sliding Window, Hash Table, Two Pointers
+
+Given a string `s`, you need to find the **length** of the longest substring that contains **no duplicate characters**.
+
+A **substring** is a contiguous sequence of characters within a string.  
+The goal is to identify the **maximum possible length** of such a substring in the given input string.
+
