d22b860e22a8a82fca9cc007613e61a39ef32520
Avinash-Dev-CFG <dogiparthi.naga@amadeus.com>
2025-08-28 11:49:54 +0530

    Solution for word pattern using hashmap

---
+++ ProblemSolvingUsingPatterns/src/F_HashMap/F_WordPattern_290/Solution.java
@@ -0,0 +1,36 @@
+package F_HashMap.F_WordPattern_290;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+class Solution {
+
+    public static void main(String[] args) {
+
+        boolean isValid = wordPattern("abba","dog cat cat dog");
+        System.out.println(isValid);
+
+    }
+    public static boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) return false;
+
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> used = new HashSet<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(w)) return false;
+            } else {
+                if (used.contains(w)) return false;
+                map.put(c, w);
+                used.add(w);
+            }
+        }
+
+        return true;
+    }
+}
