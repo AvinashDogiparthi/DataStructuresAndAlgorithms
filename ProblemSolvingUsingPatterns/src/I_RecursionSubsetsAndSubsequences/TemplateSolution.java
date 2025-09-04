a2d7095d4c6559572d006fae336165f21f18d31d
AvinashDogiparthi <dvsubharao1963@gmail.com>
2025-09-04 08:31:59 +0530

    Subsequence problems solving template solution using recursion

---
+++ ProblemSolvingUsingPatterns/src/I_RecursionSubsetsAndSubsequences/TemplateSolution.java
@@ -0,0 +1,26 @@
+package I_RecursionSubsetsAndSubsequences;
+
+public class TemplateSolution {
+
+    public static void main(String[] args) {
+        getSubSequences("","abc");
+    }
+
+    public static void getSubSequences(String p,String up){
+
+        if(up.isEmpty()){
+
+            // ignoring empty subsequence
+            if(!p.isEmpty()){
+                System.out.println(p);
+            }
+
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        getSubSequences(p + ch,up.substring(1));
+        getSubSequences(p,up.substring(1));
+    }
+}
