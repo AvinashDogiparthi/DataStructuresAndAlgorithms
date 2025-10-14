0ecfcac6706aea30c803e5225cc6ac7b636ad5ac
Avinash-Dev-CFG <dogiparthi.naga@amadeus.com>
2025-10-14 20:00:22 +0530

    BFS in adjacency matrix

--- ProblemSolvingUsingPatterns/src/K_Graphs/Concepts/C_BFS/AdjacencyMatrixImplementation.java
+++ ProblemSolvingUsingPatterns/src/K_Graphs/Concepts/C_BFS/AdjacencyMatrixImplementation.java
@@ -1,4 +1,30 @@
 package K_Graphs.Concepts.C_BFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class AdjacencyMatrixImplementation {
+
+    static void bfsTraversal(int[][] graph, int start){
+
+        boolean[] visited  = new boolean[graph.length];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()){
+
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for(int i = 0;i<graph.length;i++){
+
+                if(graph[node][i] == 1 && !visited[i]){
+                    visited[i] = true;
+                    queue.offer(i);
+                }
+            }
+        }
+    }
 }
